18086887644c1d4b50867fa4a64a62f31446acc9
DebMs123 <142893580+DebMs123@users.noreply.github.com>
2025-05-30 00:20:17 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,11 @@ const server = new McpServer({
       description: "Get a random Yo Mama joke",
       parameters: {},
     },
+    {
+      name: "get-power-joke",
+      description: "Get a random Power joke",
+      parameters: {},
+    },
   ],
 });
 
@@ -104,6 +109,26 @@ const getYoMamaJoke = server.tool(
         {
           type: "text",
           text: data.joke,
+        },
+      ],
+    };
+  }
+);
+
+// Get Power joke tool
+const getPowerJoke = server.tool(
+  "get-power-joke",
+  "Get a random Power joke",
+  async () => {
+    const response = await fetch(
+      "https://prod-175.westus.logic.azure.com:443/workflows/ef240e9705ca470c9102158cc9f00f09/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=QvEO0KDDeDSkmMd5Juog-f54qJ7TZubYij8TnNlJ-6s"
+    );
+    const data = await response.json();
+    return {
+      content: [
+        {
+          type: "text",
+          text: data.value,
         },
       ],
     };
