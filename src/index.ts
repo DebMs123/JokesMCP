f2e382aabe77769191a829dfdbaaa5038f48d881
DebMs123 <142893580+DebMs123@users.noreply.github.com>
2025-05-29 23:42:27 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
-import fetch from "node-fetch"; 
 
 const server = new McpServer({
   name: "jokesMCP",
