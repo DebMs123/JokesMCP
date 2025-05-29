cb46a6ba82d94219dd3008d8541844141037a477
DebMs123 <142893580+DebMs123@users.noreply.github.com>
2025-05-30 00:13:45 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -27,20 +27,17 @@ const server = new McpServer({
       description: "Get a random Yo Mama joke",
       parameters: {},
     },
-    {
-      name: "get-power-joke",
-      description: "Get a random power joke",
-      parameters: {},
-    },
   ],
 });
 
 // Get Chuck Norris joke tool
 const getChuckJoke = server.tool(
   "get-chuck-joke",
-  "Get a random Chuck Norris joke",
+  "Get a random Chuck Norris joke from the Dev category",
   async () => {
-    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    const response = await fetch(
+      "https://api.chucknorris.io/jokes/random?category=dev"
+    );
     const data = await response.json();
     return {
       content: [
@@ -107,26 +104,6 @@ const getYoMamaJoke = server.tool(
         {
           type: "text",
           text: data.joke,
-        },
-      ],
-    };
-  }
-);
-
-// Get Power joke tool
-const getPowerJoke = server.tool(
-  "get-power-joke",
-  "Get a random power joke",
-  async () => {
-    const response = await fetch("https://prod-175.westus.logic.azure.com:443/workflows/ef240e9705ca470c9102158cc9f00f09/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=QvEO0KDDeDSkmMd5Juog-f54qJ7TZubYij8TnNlJ-6s" 
-   );
-
-    const data = await response.json();
-    return {
-      content: [
-        {
-          type: "text",
-          text: data.value,
         },
       ],
     };
